06f86ad37fb73a19371632eb1b70feb47fcf69e4
fancitron@gmail.com <fancitron@gmail.com@22e9ff8d-b141-304c-ef59-c19e6cc553c6>
2012-11-08 04:37:10 +0000

    Owner-draw the list view items to display images.

---
+++ trunk/FileItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+
+namespace QQGameRes
+{
+    /// <summary>
+    /// Represents a physical file (usually a MIF image) to be previewed.
+    /// </summary>
+    class FileItem
+    {
+        public FileInfo FileInfo;
+        public Image Thumbnail;
+        public bool MultiFrame;
+    }
+}
