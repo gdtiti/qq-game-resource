0b46d4586b9b65cf60cbe1e6975ca8f1fade9891
fancitron <fancitron@gmail.com>
2012-11-08 00:09:15 +0000

    Backup the code that supports a TreeView of the folder structure.

---
+++ FileFolder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace QQGameRes
+{
+    /// <summary>
+    /// Represents a physical folder in the file system.
+    /// </summary>
+    public class FileFolder
+    {
+        DirectoryInfo dir;
+        FileFolder[] subFolders;
+
+        public FileFolder(string path)
+        {
+            dir = new DirectoryInfo(path);
+        }
+
+        public FileFolder[] GetSubFolders()
+        {
+            if (subFolders == null)
+            {
+                DirectoryInfo[] subdirs = dir.GetDirectories();
+                subFolders = new FileFolder[subdirs.Length];
+                for (int i = 0; i < subdirs.Length; i++)
+                {
+                    subFolders[i] = new FileFolder(subdirs[i].FullName);
+                }
+            }
+            return subFolders;
+        }
+
+        public FileInfo[] GetFiles()
+        {
+            return dir.GetFiles("*.mif");
+        }
+
+        public string Path
+        {
+            get { return dir.FullName; }
+        }
+
+        public string Name
+        {
+            get { return dir.Name; }
+        }
+    }
+}
---
+++ Repository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.IO;
+
+namespace QQGameRes
+{
+    public class Repository
+    {
+        public static string GetInstallationPath()
+        {
+            RegistryKey key = 
+                Registry.CurrentUser.OpenSubKey(@"Software\Tencent\QQGame\SYS");
+            if (key != null)
+            {
+                object val = key.GetValue("GameDirectory");
+                if (val != null && val is string)
+                    return (string)val;
+            }
+            return null;
+        }
+
+        List<FileInfo> imgFiles;
+        List<FileInfo> pkgFiles;
+
+        public Repository(string path)
+        {
+            // Search the directory recursively for supported files.
+            imgFiles = new List<FileInfo>();
+            pkgFiles = new List<FileInfo>();
+            SearchForSupportedFiles(new DirectoryInfo(path));
+        }
+
+        private void SearchForSupportedFiles(DirectoryInfo dir)
+        {
+            foreach (FileInfo f in dir.GetFiles())
+            {
+                string ext = f.Extension.ToLowerInvariant();
+                if (ext == ".mif")
+                    imgFiles.Add(f);
+                else if (ext == ".pkg")
+                    pkgFiles.Add(f);
+            }
+
+            foreach (DirectoryInfo d in dir.GetDirectories())
+            {
+                SearchForSupportedFiles(d);
+            }
+        }
+
+        public IEnumerable<FileInfo> ImageFiles
+        {
+            get { return imgFiles; }
+        }
+
+        public IEnumerable<FileInfo> PackageFiles
+        {
+            get { return pkgFiles; }
+        }
+    }
+}
