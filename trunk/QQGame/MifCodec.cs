bf2a52c88c9978a18e71be9f7b7545b50088ea8e
fancitron@gmail.com <fancitron@gmail.com@22e9ff8d-b141-304c-ef59-c19e6cc553c6>
2012-12-26 06:21:43 +0000

    Create QQGame assembly to handle MIF image decoding.

---
+++ trunk/QQGame/MifCodec.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace QQGame
+{
+    /// <summary>
+    /// Represents a frame in a MIF image.
+    /// </summary>
+    public class MifFrame
+    {
+        /// <summary>
+        /// Gets or sets the image to display for the frame.
+        /// </summary>
+        public System.Drawing.Image Image { get; set; }
+
+        /// <summary>
+        /// Gets or sets the delay, in milliseconds, before displaying the 
+        /// next frame.
+        /// </summary>
+        public int Delay { get; set; }
+    }
+
+    /// <summary>
+    /// Defines a decoder for MIF images.
+    /// </summary>
+    public class MifDecoder : IDisposable
+    {
+        private BinaryReader reader;
+        private MifHeader header;
+
+        /// <summary>
+        /// Creates a MIF image decoder from the specified data stream. 
+        /// </summary>
+        /// <param name="stream">A stream that contains the data for this
+        /// image.</param>
+        /// <exception cref="OutOfMemoryException">The stream does not 
+        /// contain a valid MIF image format.</exception>
+        /// <remarks>The stream must remain open throughout the lifetime of
+        /// this decoder, and will be automatically closed when the decoder
+        /// is disposed.</remarks>
+        public MifDecoder(Stream stream)
+        {
+            // Create a binary reader on the stream. The stream will be closed
+            // automatically when the reader is disposed.
+            reader = new BinaryReader(stream);
+
+            // Read MIF header.
+            header.Version = reader.ReadInt32();
+            header.ImageWidth = reader.ReadInt32();
+            header.ImageHeight = reader.ReadInt32();
+            header.Type = reader.ReadInt32();
+            header.FrameCount = reader.ReadInt32();
+
+            // Validate header fields.
+            if (header.Version != 0 && header.Version != 1)
+                throw new OutOfMemoryException("MIF version " + header.Version + " is not supported.");
+            if (header.ImageWidth <= 0)
+                throw new OutOfMemoryException("ImageWidth field must be positive.");
+            if (header.ImageHeight <= 0)
+                throw new OutOfMemoryException("ImageHeight field must be positive.");
+            if (header.Type != 3 && header.Type != 7)
+                throw new OutOfMemoryException("MIF type " + header.Type + " is not supported.");
+            if (header.FrameCount <= 0)
+                throw new OutOfMemoryException("FrameCount field must be positive.");
+        }
+
+        /// <summary>
+        /// Gets the number of frames in this image.
+        /// </summary>
+        public int FrameCount
+        {
+            get { return header.FrameCount; }
+        }
+
+        /// <summary>
+        /// Gets the width of the image in pixels.
+        /// </summary>
+        public int ImageWidth
+        {
+            get { return header.ImageWidth; }
+        }
+
+        /// <summary>
+        /// Gets the height of the image in pixels.
+        /// </summary>
+        public int ImageHeight
+        {
+            get { return header.ImageHeight; }
+        }
+
+        /// <summary>
+        /// Decodes the next frame from the underlying stream.
+        /// </summary>
+        /// <returns>The frame decoded, or <code>null</code> if there are
+        /// no more frames left.</returns>
+        /// <exception cref="ObjectDisposedException">This decoder has been
+        /// disposed.</exception>
+        /// <exception cref="OutOfMemoryException">The stream does not 
+        /// contain a valid MIF image format.</exception>
+        /// <remarks>The caller is responsible for disposing the returned
+        /// <code>System.Drawing.Image</code>.</remarks>
+        public MifFrame DecodeFrame()
+        {
+            if (reader == null)
+                throw new ObjectDisposedException("MifDecoder");
+
+#if false
+            // TODO: do we need this?
+            // Return null if there are no more frames left.
+            if (frameIndex + 1 >= header.FrameCount)
+            {
+                reader.Close();
+                reader = null;
+                return false;
+            }
+#endif
+
+            // Read the next image from the stream.
+            // Create a buffer to read the raw data and another buffer to
+            // convert the data.
+            int width = header.ImageWidth;
+            int height = header.ImageHeight;
+            byte[] buffer = new byte[3 * width * height];
+            byte[] bmpData = new byte[4 * width * height];
+
+            // Read frame delay if Type is 7.
+            MifFrame frame = new MifFrame();
+            if (header.Type == 7)
+                frame.Delay = reader.ReadInt32();
+            else
+                frame.Delay = 0;
+
+            // Load frame into memory.
+            if (reader.Read(buffer, 0, buffer.Length) != buffer.Length)
+                throw new OutOfMemoryException("Premature end of file.");
+
+            // Convert each pixel of the frame. Note that we assume that
+            // Stride is equal to 4 * Width, i.e. there's no padding.
+            // If we choose to loose this assumption, we will need to 
+            // convert the image scan line by scan line.
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    byte a = buffer[2 * width * height + y * width + x];
+                    byte b1 = buffer[2 * (y * width + x)];
+                    byte b2 = buffer[2 * (y * width + x) + 1];
+                    byte alpha = (byte)((a >= 32) ? 255 : (a << 3));
+                    byte red = (byte)(b2 & 0xF8);
+                    byte green = (byte)(((b2 << 5) | (b1 >> 3)) & 0xFC);
+                    byte blue = (byte)((b1 & 0x1F) << 3);
+
+                    bmpData[4 * (y * width + x) + 0] = blue;
+                    bmpData[4 * (y * width + x) + 1] = green;
+                    bmpData[4 * (y * width + x) + 2] = red;
+                    bmpData[4 * (y * width + x) + 3] = alpha;
+                }
+            }
+
+            // Create a bitmap from the converted pixel data.
+            Bitmap bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, width, height),
+                ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+            Marshal.Copy(bmpData, 0, data.Scan0, 4 * width * height);
+            bmp.UnlockBits(data);
+
+            // Return this frame.
+            frame.Image = bmp;
+            return frame;
+        }
+
+        /// <summary>
+        /// Disposes the decoder and closes the underlying stream.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (reader == null) // already disposed
+                return;
+            if (disposing)
+                reader.Close();
+            reader = null;
+        }
+
+#if false
+            // Count the number of colors and green bits in the bitmap.
+            bool[] colorPresent = new bool[65536];
+            int greenBit = 0;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    byte b1 = buffer[2 * (y * width + x)];
+                    byte b2 = buffer[2 * (y * width + x) + 1];
+                    byte green = (byte)(((b2 << 5) | (b1 >> 3)) & 0xFC);
+                    colorPresent[(b2 << 8) | b1] = true;
+                    if ((green & 4) != 0)
+                        greenBit++;
+                }
+            }
+
+            int colorCount = 0;
+            for (int i = 0; i < 65536; i++)
+            {
+                if (colorPresent[i])
+                    ++colorCount;
+            }
+            System.Diagnostics.Debug.WriteLine("Number of colors: " + colorCount);
+            System.Diagnostics.Debug.WriteLine("Green bit set in " + greenBit + " pixels.");
+#endif
+    }
+
+    /// <summary>
+    /// Represents the header of an MIF image.
+    /// </summary>
+    public struct MifHeader
+    {
+        /// <summary>
+        /// Version of this image; must be 0 or 1.
+        /// </summary>
+        public int Version;
+
+        /// <summary>
+        /// Width of an individual image, in pixels.
+        /// </summary>
+        public int ImageWidth;
+
+        /// <summary>
+        /// Height of an individual image, in pixels.
+        /// </summary>
+        public int ImageHeight;
+
+        /// <summary>
+        /// Type of the MIF image; 3 for single-frame, 7 for multi-frame.
+        /// </summary>
+        public int Type;
+
+        /// <summary>
+        /// Number of frames in this image.
+        /// </summary>
+        public int FrameCount;
+    }
+
+
+}
--- trunk/QQGameRes/MifImage.cs
+++ trunk/QQGameRes/MifImage.cs
@@ -9,43 +9,11 @@ using System.Runtime.InteropServices;
 namespace QQGameRes
 {
     /// <summary>
-    /// Represents the header of an MIF image.
-    /// </summary>
-    public struct MifHeader
-    {
-        /// <summary>
-        /// Version of this image; must be 0 or 1.
-        /// </summary>
-        public int Version;
-
-        /// <summary>
-        /// Width of an individual image, in pixels.
-        /// </summary>
-        public int ImageWidth;
-
-        /// <summary>
-        /// Height of an individual image, in pixels.
-        /// </summary>
-        public int ImageHeight;
-
-        /// <summary>
-        /// Type of the MIF image; 3 for single-frame, 7 for multi-frame.
-        /// </summary>
-        public int Type;
-
-        /// <summary>
-        /// Number of frames in this image.
-        /// </summary>
-        public int FrameCount;
-    }
-
-    /// <summary>
     /// Represents a multi-frame MIF image.
     /// </summary>
     public class MifImage : AnimationImage, IDisposable
     {
-        private BinaryReader reader;
-        private MifHeader header;
+        private QQGame.MifDecoder decoder;
         private int frameIndex;
         private AnimationFrame currentFrame;
 
@@ -55,31 +23,9 @@ namespace QQGameRes
         /// <param name="stream">The stream from which the image is loaded.</param>
         public MifImage(Stream stream)
         {
-            // Create a binary reader on the stream. The stream will be closed
-            // automatically when the reader is disposed.
-            reader = new BinaryReader(stream);
-
-            // Read MIF header.
-            header.Version = reader.ReadInt32();
-            header.ImageWidth = reader.ReadInt32();
-            header.ImageHeight = reader.ReadInt32();
-            header.Type = reader.ReadInt32();
-            header.FrameCount = reader.ReadInt32();
-
-            // Validate header fields.
-            if (header.Version != 0 && header.Version != 1)
-                throw new IOException("MIF version " + header.Version + " is not supported.");
-            if (header.ImageWidth <= 0)
-                throw new IOException("ImageWidth field must be positive.");
-            if (header.ImageHeight <= 0)
-                throw new IOException("ImageHeight field must be positive.");
-            if (header.Type != 3 && header.Type != 7)
-                throw new IOException("MIF type " + header.Type + " is not supported.");
-            if (header.FrameCount <= 0)
-                throw new IOException("FrameCount field must be positive.");
-
-            // Set frame counter to -1 (before loading the first frame).
-            frameIndex = -1;
+            this.decoder = new QQGame.MifDecoder(stream);
+            this.frameIndex = -1;
+            this.currentFrame = null;
         }
 
         /// <summary>
@@ -87,7 +33,7 @@ namespace QQGameRes
         /// </summary>
         public int FrameCount
         {
-            get { return header.FrameCount; }
+            get { return decoder.FrameCount; }
         }
 
         /// <summary>
@@ -113,7 +59,7 @@ namespace QQGameRes
         /// </summary>
         public int Width
         {
-            get { return header.ImageWidth; }
+            get { return decoder.ImageWidth; }
         }
 
         /// <summary>
@@ -121,7 +67,7 @@ namespace QQGameRes
         /// </summary>
         public int Height
         {
-            get { return header.ImageHeight; }
+            get { return decoder.ImageHeight; }
         }
 
         /// <summary>
@@ -131,181 +77,30 @@ namespace QQGameRes
         /// <code>false</code> if there are no more frames left.</returns>
         public bool GetNextFrame()
         {
-            if (reader == null)
-                return false;
+
+            //if (reader == null)
+            //    return false;
 
             // Return null if there are no more frames left.
-            if (frameIndex + 1 >= header.FrameCount)
+            if (frameIndex + 1 >= FrameCount)
             {
-                reader.Close();
-                reader = null;
+                Dispose();
                 return false;
             }
 
-            // Read the next image from the stream.
-            int width = header.ImageWidth;
-            int height = header.ImageHeight;
-            int bytesPerImage = width * height * 3;
-            byte[] buffer = new byte[bytesPerImage];
-
-            // Read frame delay if Type is 7.
-            AnimationFrame frame = new AnimationFrame();
-            if (header.Type == 7)
-                frame.Delay = reader.ReadInt32();
-            else
-                frame.Delay = 0;
-
-#if false
-            if (frame.Delay != 0 && frame.Delay != 100)
-                System.Diagnostics.Debug.WriteLine("Special frame delay: " + frame.Delay);
-#endif
-
-            // Load image into memory buffer.
-            if (reader.Read(buffer, 0, bytesPerImage) != bytesPerImage)
-                throw new IOException("Premature end of file.");
-
-            // Create a bitmap by setting its data directly.
-            byte[] bmpData = new byte[4 * width * height];
-            for (int y = 0; y < height; y++)
+            QQGame.MifFrame frame=decoder.DecodeFrame();
+            currentFrame=new AnimationFrame
             {
-                for (int x = 0; x < width; x++)
-                {
-                    byte a = buffer[2 * width * height + y * width + x];
-                    byte b1 = buffer[2 * (y * width + x)];
-                    byte b2 = buffer[2 * (y * width + x) + 1];
-                    byte alpha = (byte)((a >= 32) ? 255 : (a << 3));
-                    byte red = (byte)(b2 & 0xF8);
-                    byte green = (byte)(((b2 << 5) | (b1 >> 3)) & 0xFC);
-                    byte blue = (byte)((b1 & 0x1F) << 3);
-
-                    bmpData[4 * (y * width + x) + 0] = blue;
-                    bmpData[4 * (y * width + x) + 1] = green;
-                    bmpData[4 * (y * width + x) + 2] = red;
-                    bmpData[4 * (y * width + x) + 3] = alpha;
-                }
-            }
-
-            Bitmap bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
-            BitmapData data = bmp.LockBits(new Rectangle(0, 0, width, height), 
-                ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
-            Marshal.Copy(bmpData, 0, data.Scan0, 4 * width * height);
-            bmp.UnlockBits(data);
-
-            frame.Image = bmp;
-
-            // Update the current frame.
+                 Image=frame.Image,
+                 Delay=frame.Delay
+            };
             frameIndex++;
-            currentFrame = frame;
             return true;
         }
 
-#if false
-        public IEnumerable<AnimationFrame> GetFrames()
-        {
-            if (reader == null)
-                throw new ObjectDisposedException("MifImage");
-
-            // Create a buffer to read the frame and another buffer to process
-            // bitmap data.
-            int width = header.ImageWidth;
-            int height = header.ImageHeight;
-            byte[] buffer = new byte[3*width*height];
-            byte[] bmpData = new byte[4 * width * height];
-
-            // Read each frame in turn.
-            for (int i = 0; i < header.FrameCount; i++)
-            {
-                // Read frame delay if Type is 7.
-                AnimationFrame frame = new AnimationFrame();
-                if (header.Type == 7)
-                    frame.Delay = reader.ReadInt32();
-                else
-                    frame.Delay = 0;
-
-                // Load frame into memory.
-                if (reader.Read(buffer, 0, buffer.Length) != buffer.Length)
-                    throw new IOException("Premature end of file.");
-
-                // Convert each pixel of the frame. Note that we assume that
-                // Stride is equal to 4 * Width, i.e. there's no padding.
-                // If we choose to loose this assumption, we will need to 
-                // convert the image scan line by scan line.
-                for (int y = 0; y < height; y++)
-                {
-                    for (int x = 0; x < width; x++)
-                    {
-                        byte a = buffer[2 * width * height + y * width + x];
-                        byte b1 = buffer[2 * (y * width + x)];
-                        byte b2 = buffer[2 * (y * width + x) + 1];
-                        byte alpha = (byte)((a >= 32) ? 255 : (a << 3));
-                        byte red = (byte)(b2 & 0xF8);
-                        byte green = (byte)(((b2 << 5) | (b1 >> 3)) & 0xFC);
-                        byte blue = (byte)((b1 & 0x1F) << 3);
-
-                        bmpData[4 * (y * width + x) + 0] = blue;
-                        bmpData[4 * (y * width + x) + 1] = green;
-                        bmpData[4 * (y * width + x) + 2] = red;
-                        bmpData[4 * (y * width + x) + 3] = alpha;
-                    }
-                }
-
-                // Create a bitmap from the converted pixel data.
-                Bitmap bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
-                BitmapData data = bmp.LockBits(new Rectangle(0, 0, width, height),
-                    ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
-                Marshal.Copy(bmpData, 0, data.Scan0, 4 * width * height);
-                bmp.UnlockBits(data);
-
-                // Return this frame.
-                frame.Image = bmp;
-                yield return frame;
-            }
-
-            // After we finish, close the stream.
-            reader.Close();
-            reader = null;
-        }
-#endif
-
         public void Dispose()
         {
-            Dispose(true);
+            decoder.Dispose();
         }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (reader == null)
-                return;
-            if (disposing)
-                reader.Close();
-            reader = null;
-        }
-
-#if false
-            // Count the number of colors and green bits in the bitmap.
-            bool[] colorPresent = new bool[65536];
-            int greenBit = 0;
-            for (int y = 0; y < height; y++)
-            {
-                for (int x = 0; x < width; x++)
-                {
-                    byte b1 = buffer[2 * (y * width + x)];
-                    byte b2 = buffer[2 * (y * width + x) + 1];
-                    byte green = (byte)(((b2 << 5) | (b1 >> 3)) & 0xFC);
-                    colorPresent[(b2 << 8) | b1] = true;
-                    if ((green & 4) != 0)
-                        greenBit++;
-                }
-            }
-
-            int colorCount = 0;
-            for (int i = 0; i < 65536; i++)
-            {
-                if (colorPresent[i])
-                    ++colorCount;
-            }
-            System.Diagnostics.Debug.WriteLine("Number of colors: " + colorCount);
-            System.Diagnostics.Debug.WriteLine("Green bit set in " + greenBit + " pixels.");
-#endif
     }
 }
