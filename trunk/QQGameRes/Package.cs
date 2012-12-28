b7d1747aa82a06a2f9f4010b6296d9f11f9ad83e
fancitron@gmail.com <fancitron@gmail.com@22e9ff8d-b141-304c-ef59-c19e6cc553c6>
2012-12-28 09:26:07 +0000

    Move PkgArchive class to QQGame assembly.

--- trunk/QQGameRes/Package.cs
+++ trunk/QQGameRes/Package.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 
 namespace QQGameRes
 {
+#if false
     public struct PackageEntryInfo
     {
         public string Path;
@@ -13,87 +15,29 @@ namespace QQGameRes
         public uint Size;
         public uint OriginalSize;
     }
+#endif
 
     public class Package : ResourceFolder
     {
+        private QQGame.PkgArchive ar;
         private string filename;
         private PackageEntry[] entries;
 
+        // TODO: dispose ar when not used.
         public Package(string filename)
         {
             this.filename = filename;
-
-            using (BinaryReader reader = new BinaryReader(
-                new FileStream(filename, FileMode.Open, FileAccess.Read)))
-            {
-                // Read file header (16 bytes):
-                // 0-3: file signature (0x64)
-                // 4-7: number of files
-                // 8-11: offset of file name area.
-                // 12-15: size of file name area.
-                if (reader.ReadInt32() != 0x64)
-                    throw new IOException("File signature mismatch.");
-                uint numFiles = reader.ReadUInt32();
-                uint indexOffset = reader.ReadUInt32();
-                uint indexSize = reader.ReadUInt32();
-
-                // Read file names. For each file name entry, the format is:
-                // 2 bytes: (X) length of file name, in characters
-                // X or 2X bytes: file name (encoded in GBK or UCS2)
-                // 4 bytes: all zero; this marks the end of file name
-                // 4 bytes: file offset
-                // 4 bytes: original file size
-                // 4 bytes: file size (packed)
-                reader.BaseStream.Seek(indexOffset, SeekOrigin.Begin);
-                List<PackageEntry> entryList = new List<PackageEntry>();
-                byte[] buffer = new byte[65536 * 2];
-                for (uint i = 0; i < numFiles; i++)
-                {
-                    PackageEntry entry = new PackageEntry();
-                    entry.PackagePath = filename;
-
-                    ushort length = reader.ReadUInt16();
-                    if (reader.Read(buffer, 0, length + 4) != length + 4)
-                        throw new IOException("Premature end of file.");
-                    if (buffer[length] == 0 && buffer[length + 1] == 0 &&
-                        buffer[length + 2] == 0 && buffer[length + 3] == 0)
-                    {
-                        // Encoded in GBK
-                        entry.EntryPath = Encoding.GetEncoding("GBK").GetString(buffer, 0, length);
-                    }
-                    else
-                    {
-                        // Encoded in UCS-2
-                        if (reader.Read(buffer, length + 4, length) != length)
-                            throw new IOException("Premature end of file.");
-                        entry.EntryPath = Encoding.Unicode.GetString(buffer, 0, length * 2);
-                    }
-
-                    entry.EntryOffset = reader.ReadUInt32();
-                    entry.OriginalSize = reader.ReadUInt32();
-                    entry.EntrySize = reader.ReadUInt32();
-                    entryList.Add(entry);
-                }
-                this.entries = entryList.ToArray();
-            }
+            this.ar = new QQGame.PkgArchive(filename);
+            this.entries = (from ent in ar.Entries
+                            select new PackageEntry(ent)
+                            {
+                                PackagePath = filename,
+                                EntryPath = ent.FullName,
+                                EntrySize = ent.CompressedLength,
+                                EntryOffset = 0,
+                                OriginalSize = ent.Length
+                            }).ToArray();
         }
-
-#if false
-        public Stream ExtractEntry(int index)
-        {
-            uint offset = entries[index].Offset;
-            uint size = entries[index].Size;
-            if (size > stream.Length)
-                throw new IOException("Invalid size field.");
-
-            // Seek to file content and skip two bytes of ZLIB header
-            // which is not recognized by .NET's DeflateStream impl.
-            stream.Seek(offset + 2, SeekOrigin.Begin);
-            
-            // Create and return a DeflateStream from here.
-            return new DeflateStream(stream, CompressionMode.Decompress, true);
-        }
-#endif
 
         public string Name
         {
@@ -103,6 +47,37 @@ namespace QQGameRes
         public ResourceEntry[] Entries
         {
             get { return this.entries; }
+        }
+    }
+
+    public class PackageEntry : ResourceEntry
+    {
+        private QQGame.PkgArchiveEntry entry;
+
+        public PackageEntry(QQGame.PkgArchiveEntry ent)
+        {
+            entry = ent;
+        }
+
+        public string PackagePath;
+        public string EntryPath;
+        public int EntryOffset;
+        public int EntrySize;
+        public int OriginalSize;
+
+        public string Name
+        {
+            get { return PackagePath + "\\" + EntryPath; }
+        }
+
+        public int Size
+        {
+            get { return (int)OriginalSize; }
+        }
+
+        public Stream Open()
+        {
+            return entry.Open();
         }
     }
 }
