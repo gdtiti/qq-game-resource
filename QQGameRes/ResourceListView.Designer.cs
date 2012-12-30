3f0151b98d3f845e9cac516ffa68d8cbd1f0c6eb
fancitron <fancitron@gmail.com>
2012-12-30 13:40:56 +0000

    Refactor shared classes.

--- QQGameRes/ResourceListView.Designer.cs
+++ QQGameRes/ResourceListView.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.imageListPreview = new System.Windows.Forms.ImageList(this.components);
-            this.lvEntries = new ControlExtensions.DoubleBufferedListView();
+            this.lvEntries = new Util.Forms.DoubleBufferedListView();
             this.animator = new QQGameRes.AnimationPlayer(this.components);
             this.SuspendLayout();
             // 
@@ -74,7 +74,7 @@
 
         #endregion
 
-        private ControlExtensions.DoubleBufferedListView lvEntries;
+        private Util.Forms.DoubleBufferedListView lvEntries;
         private System.Windows.Forms.ImageList imageListPreview;
         private AnimationPlayer animator;
     }
